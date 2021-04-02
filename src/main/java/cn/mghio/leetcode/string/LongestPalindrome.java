e5f5859c074e30b5fa2ce8e8439a717e8db44110
mghio <mghio.dev@gmail.com>
2021-04-03 00:07:20 +0800

    string: Longest palindrome solution 2.

--- src/main/java/cn/mghio/leetcode/string/LongestPalindrome.java
+++ src/main/java/cn/mghio/leetcode/string/LongestPalindrome.java
@@ -8,14 +8,73 @@ package cn.mghio.leetcode.string;
  */
 public class LongestPalindrome {
 
-  /**
-   * 方法一：暴力解法
-   */
   public String longestPalindrome(String s) {
     if (s == null || s.length() < 2) {
       return s;
     }
 
+    // 方法一：暴力解法
+//    String result = violentSolution(s);
+
+    // 方法二：中心扩散法
+    String result = expandCenter(s);
+
+    return result;
+  }
+
+  /**
+   * 方法二：中心扩散法
+   */
+  private String expandCenter(String s) {
+    int maxLen = 1;
+    int begin = 0;
+    char[] charArray = s.toCharArray();
+    for (int i = 0, len = s.length(); i < len - 1; i++) {
+      int oddLen = expandAroundCenter(charArray, i, i);
+      int evenLen = expandAroundCenter(charArray, i, i + 1);
+
+      int curMaxLen = Math.max(oddLen, evenLen);
+      if (curMaxLen > maxLen) {
+        maxLen = curMaxLen;
+        // 这一步需要在纸上画图发现规律
+        begin = i - (maxLen - 1) / 2;
+      }
+    }
+
+    return s.substring(begin, begin + maxLen);
+  }
+
+  /**
+   * @param charArray 原始字符串的字符数组
+   * @param left      起始左边界（可以取到）
+   * @param right     起始右边界（可以取到）
+   * @return 回文串的长度
+   */
+  private int expandAroundCenter(char[] charArray, int left, int right) {
+    // 当 left = right 的时候，回文串的中心是一个字符，回文串的长度是一个奇数
+    // 当 left = right + 1 的时候，此时回文串的中心是两个字符，回文串的长度是偶数
+    int len = charArray.length;
+    int i = left;
+    int j = right;
+
+    while (i >= 0 && j < len) {
+      if (charArray[i] == charArray[j]) {
+        i--;
+        j++;
+      } else {
+        break;
+      }
+    }
+
+    // 跳出 while 循环时，恰好满足 s.chatAt(i) != s.chatAt(j)
+    // 回文串的长度是 j - i + 1 - 2 = j - i - 1
+    return j - i - 1;
+  }
+
+  /**
+   * 方法一：暴力解法
+   */
+  private String violentSolution(String s) {
     int maxLen = 1;
     int begin = 0;
     // s.chatAt(i) 每次都会检查数组下标越界，因此先转换成字符数组，这一步非必须
@@ -24,8 +83,9 @@ public class LongestPalindrome {
     // 枚举所有长度大于 1 的子串
     for (int i = 0, len = s.length(); i < len - 1; i++) {
       for (int j = i + 1; j < len; j++) {
-        if (j - i + 1 > maxLen && validPalindrome(charArray, i, j)) {
-          maxLen = j - i + 1;
+        int curMaxLen = j - i + 1;
+        if (curMaxLen > maxLen && validPalindrome(charArray, i, j)) {
+          maxLen = curMaxLen;
           begin = i;
         }
       }
