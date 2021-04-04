e0f4a33ad13076fff86a89977e9c855deb03ef86
mghio <mghio.dev@gmail.com>
2021-04-04 16:56:08 +0800

    string: reverse words solution 2.

--- src/main/java/cn/mghio/leetcode/string/ReverseWords.java
+++ src/main/java/cn/mghio/leetcode/string/ReverseWords.java
@@ -28,11 +28,100 @@ public class ReverseWords {
 
     // 方法一：使用语言特性
 //    String result = getReverseWordWithLanguageFeature1(s);
-    String result = getReverseWordWithLanguageFeature2(s);
+//    String result = getReverseWordWithLanguageFeature2(s);
+
+    // 方法二：自行编写方法实现
+    // Step 1: 去除多余空格，并转换为可变字符串（StringBuilder）
+    String result = customAlgo(s);
 
     return result;
   }
 
+  /**
+   * 方法二：自行编写方法实现
+   *
+   * 时间复杂度：O(N)，其中 N 为输入字符串的长度。
+   * 空间复杂度：需要 O(N) 的空间来存储字符串，而 C++ 方法因为字符串可变只需要 O(1)O(1) 的额外空间来存放若干变量。
+   */
+  private String customAlgo(String s) {
+    // Step 1: 去除首尾空格并转换为可变的字符串
+    StringBuilder sb = trimSpaceAndConvertToStringBuilder(s);
+    // Step 2: 翻转字符串
+    reverse(sb, 0, sb.length() - 1);
+    // Step 3: 翻转每个单词
+    reverseEachWord(sb);
+
+    String result = sb.toString();
+    return result;
+  }
+
+  /**
+   * 翻转每个单词
+   */
+  private void reverseEachWord(StringBuilder sb) {
+    int n = sb.length();
+    int start = 0;
+    int end = 0;
+    while (start < n) {
+      // 循环至单词的末尾
+      while (end < n && sb.charAt(end) != ' ') {
+        ++end;
+      }
+      // 翻转单词
+      reverse(sb, start, end - 1);
+      // 更新 start，去找下一个单词
+      start = end + 1;
+      end++;
+    }
+  }
+
+  /**
+   * 翻转字符串
+   */
+  private void reverse(StringBuilder sb, int left, int right) {
+    while (left < right) {
+      char temp = sb.charAt(left);
+      sb.setCharAt(left++, sb.charAt(right));
+      sb.setCharAt(right--, temp);
+    }
+  }
+
+  /**
+   * 去除首尾空格并转换为可变的字符串
+   */
+  private StringBuilder trimSpaceAndConvertToStringBuilder(String s) {
+    int left = 0;
+    int right = s.length() - 1;
+
+    // 去除字符串开头的空白字符
+    while (left <= right && s.charAt(left) == ' ') {
+      left++;
+    }
+    // 去除字符串结尾的空白字符
+    while (left <= right && s.charAt(right) == ' ') {
+      right--;
+    }
+
+    // 将字符串多余的空格去除
+    StringBuilder sb = new StringBuilder();
+    while (left <= right) {
+      char c = s.charAt(left);
+      if (c != ' ') {
+        sb.append(c);
+      } else if (sb.charAt(sb.length() - 1) != ' ') {
+        sb.append(c);
+      }
+      ++left;
+    }
+    return sb;
+  }
+
+  /**
+   * 方法一：使用语言特性
+   *
+   * 时间复杂度：O(N)，其中 N 为输入字符串的长度。
+   * 空间复杂度：O(N)，用来存储字符串分割之后的结果。
+   */
   private String getReverseWordWithLanguageFeature2(String s) {
     List<String> wordList = Arrays.asList(s.trim().split("\\s+"));
     Collections.reverse(wordList);
