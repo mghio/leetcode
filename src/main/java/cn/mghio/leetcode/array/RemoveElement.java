b9a0640165ab65cf7cdb2a72b5632eb3e8180b84
mghio <mghio.dev@gmail.com>
2021-04-10 00:00:30 +0800

    array: fix remove element solution 1.

--- src/main/java/cn/mghio/leetcode/array/RemoveElement.java
+++ src/main/java/cn/mghio/leetcode/array/RemoveElement.java
@@ -11,7 +11,7 @@ public class RemoveElement {
 
   public int removeElement(int[] nums, int target) {
     if (nums == null || nums.length == 0) {
-      return -1;
+      return 0;
     }
 
     int slow = 0, fast = 0;
