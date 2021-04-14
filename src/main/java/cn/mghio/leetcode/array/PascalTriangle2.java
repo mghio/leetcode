b940232d0d6dd967b572dec20028fb63b0c81636
mghio <mghio.dev@gmail.com>
2021-04-14 08:36:36 +0800

    array: optimization pascal triangle2 solution 1.

--- src/main/java/cn/mghio/leetcode/array/PascalTriangle2.java
+++ src/main/java/cn/mghio/leetcode/array/PascalTriangle2.java
@@ -19,9 +19,27 @@ public class PascalTriangle2 {
 //    List<Integer> result = recursion(rowIndex);
 
     // 方法二：优化递归（注意到对第 i+1 行的计算仅用到了第 i 行的数据，因此可以使用滚动数组的思想优化空间复杂度。）
-    List<Integer> result = optimizationRecursion(rowIndex);
+//    List<Integer> result = optimizationRecursion(rowIndex);
+
+    // 方法三：优化递归（只用一个 List 倒着计算第 i 行）
+    List<Integer> result = optimizationRecursion2(rowIndex);
 
     return result;
+  }
+
+  /**
+   * 方法三：优化递归，只用一个数组
+   */
+  private List<Integer> optimizationRecursion2(int rowIndex) {
+    List<Integer> row = new ArrayList<>();
+    row.add(1);
+    for (int i = 1; i <= rowIndex; i++) {
+      row.add(0);
+      for (int j = i; j > 0; j--) {
+        row.set(j, row.get(j - 1) + row.get(j));
+      }
+    }
+    return row;
   }
 
   /**
