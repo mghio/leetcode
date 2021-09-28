e1a29f6c22ab08a26b4096d1f1369dbc48805cc3
mghio <mghio.dev@gmail.com>
2021-09-28 09:37:59 +0800

    feat(tree): connect prefect binary tree

---
+++ src/main/java/cn/mghio/leetcode/tree/Node.java
@@ -0,0 +1,27 @@
+package java.cn.mghio.leetcode.tree;
+
+/**
+ * @author mghio
+ * @since 2021-09-28
+ */
+public class Node {
+
+  int val;
+  Node left;
+  Node right;
+  Node next;
+
+  public Node() {
+  }
+
+  public Node(int val) {
+    this.val = val;
+  }
+
+  public Node(int val, Node left, Node right, Node next) {
+    this.val = val;
+    this.left = left;
+    this.right = right;
+    this.next = next;
+  }
+}
---
+++ src/main/java/cn/mghio/leetcode/tree/PerfectBinaryTreeConnect.java
@@ -0,0 +1,37 @@
+package java.cn.mghio.leetcode.tree;
+
+/**
+ * You are given a perfect binary tree where all leaves are on the same level, and every parent has
+ * two children. The binary tree has the following definition:
+ *
+ * @author mghio
+ * @since 2021-09-28
+ */
+public class PerfectBinaryTreeConnect {
+
+  /**
+   * Connect prefect binary tree.
+   *
+   * @param root the root node
+   * @return connected prefect binary tree root node
+   */
+  public Node connect(Node root) {
+    if (root != null) {
+      dfs(root.left, root.right);
+    }
+
+    return root;
+  }
+
+  private void dfs(Node left, Node right) {
+    if (left == null || left.next == right) {
+      return;
+    }
+
+    left.next = right;
+    dfs(left.left, left.right);
+    dfs(left.right, right.left);
+    dfs(right.left, right.right);
+  }
+
+}
