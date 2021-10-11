8b4567c0bb8302efcd2ab611ee3fa9272c95192e
mghio <mghio.dev@gmail.com>
2021-10-11 22:35:35 +0800

    feat(list): remove Nth from end

---
+++ src/main/java/cn/mghio/leetcode/list/RemoveNthFromEnd.java
@@ -0,0 +1,35 @@
+package java.cn.mghio.leetcode.list;
+
+/**
+ * Given the head of a linked list, remove the nth node from the end of the list and return its.
+ * head.
+ *
+ * @author mghio
+ * @since 2021-10-11
+ */
+public class RemoveNthFromEnd {
+
+  /**
+   * remove the nth node from the end of the list and return its.
+   *
+   * @param head the head of singly-linked list.
+   * @param n the n th
+   * @return the nth node from the end of list
+   */
+  public SinglyNode removeNthFromEnd(SinglyNode head, int n) {
+    SinglyNode dummy = new SinglyNode(-1, head);
+    SinglyNode first = head;
+    SinglyNode second = dummy;
+    for (int i = 0; i < n; i++) {
+      first = first.next;
+    }
+    while (first != null) {
+      first = first.next;
+      second = second.next;
+    }
+    // delete nth node
+    second.next = second.next.next;
+    return dummy.next;
+  }
+
+}
--- src/main/java/cn/mghio/leetcode/list/SinglyNode.java
+++ src/main/java/cn/mghio/leetcode/list/SinglyNode.java
@@ -14,4 +14,9 @@ public class SinglyNode {
   public SinglyNode(int val) {
     this.val = val;
   }
+
+  public SinglyNode(int val, SinglyNode next) {
+    this.val = val;
+    this.next = next;
+  }
 }
