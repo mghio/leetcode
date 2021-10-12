eab700b28964b7850f1ce1fa039611817065dbff
mghio <mghio.dev@gmail.com>
2021-10-12 21:32:26 +0800

    feat(list): determine the linked list contains a cycle

---
+++ src/main/java/cn/mghio/leetcode/list/HasCycle.java
@@ -0,0 +1,34 @@
+package java.cn.mghio.leetcode.list;
+
+/**
+ * Determine whether the linked list contains a cycle.
+ *
+ * @author mghio
+ * @since 2021-10-12
+ */
+public class HasCycle {
+
+  /**
+   * Determine whether the linked list contains a cycle.
+   *
+   * @param head the head node
+   * @return true has cycled, else false
+   */
+  public boolean hasCycle(SinglyNode head) {
+    if (head == null) {
+      return false;
+    }
+
+    SinglyNode fast = head, slow = head;
+    while (fast != null && fast.next != null) {
+      slow = slow.next;
+      fast = fast.next.next;
+      if (slow == fast) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+}
