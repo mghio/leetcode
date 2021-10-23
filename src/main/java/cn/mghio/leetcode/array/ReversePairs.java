07bc604022643c10c127f521f8b912c552391ff2
mghio <mghio.dev@gmail.com>
2021-10-23 23:05:14 +0800

    feat(array): find the total number of reverse pairs in array solution 3

--- src/main/java/cn/mghio/leetcode/array/ReversePairs.java
+++ src/main/java/cn/mghio/leetcode/array/ReversePairs.java
@@ -1,5 +1,7 @@
 package java.cn.mghio.leetcode.array;
 
+import java.util.Arrays;
+
 /**
  * In the two numbers in the array, if the first number is greater than the following number, the
  * two numbers form a reverse pair. Enter an array and find the total number of reverse pairs in
@@ -54,54 +56,111 @@ public class ReversePairs {
     int[] copy = new int[len];
     System.arraycopy(nums, 0, copy, 0, len);
 
-    int[] temp = new int[len];
-    return reversePairs(copy, 0, len - 1, temp);
+    int[] tmp = new int[len];
+    return reversePair(copy, 0, len - 1, tmp);
   }
 
-  private int reversePairs(int[] nums, int left, int right, int[] temp) {
+  /**
+   * Given an array and find the total number of reverse pairs in this array.
+   *
+   * @param nums the array
+   * @return the total number of reverse pairs
+   */
+  public int reversePairsSolution3(int[] nums) {
+    if (nums == null) {
+      return 0;
+    }
+
+    int len = nums.length;
+    int[] tmp = new int[len];
+    System.arraycopy(nums, 0, tmp, 0, len);
+    Arrays.sort(tmp);
+
+    for (int i = 0; i < len; i++) {
+      nums[i] = Arrays.binarySearch(tmp, nums[i]) + 1;
+    }
+
+    BIT bit = new BIT(len);
+    int ans = 0;
+    for (int i = len - 1; i >= 0; i--) {
+      ans += bit.query(nums[i] - 1);
+      bit.update(nums[i]);
+    }
+    return ans;
+  }
+
+  private int reversePair(int[] nums, int left, int right, int[] tmp) {
     if (left == right) {
       return 0;
     }
 
-    int mid = left + (right - left) / 2;
-    int leftPairs = reversePairs(nums, left, mid, temp);
-    int rightPairs = reversePairs(nums, mid + 1, right, temp);
-
+    int mid = left + ((right - left) >> 2);
+    int leftPairs = reversePair(nums, left, mid, tmp);
+    int rightPairs = reversePair(nums, mid + 1, right, tmp);
     if (nums[mid] <= nums[mid + 1]) {
       return leftPairs + rightPairs;
     }
 
-    int crossPairs = mergeAndCount(nums, left, mid, right, temp);
+    int crossPairs = mergeAndCount(nums, left, mid, right, tmp);
     return leftPairs + rightPairs + crossPairs;
   }
 
-  private int mergeAndCount(int[] nums, int left, int mid, int right, int[] temp) {
+  private int mergeAndCount(int[] nums, int left, int mid, int right, int[] tmp) {
+    // left ... right: tmp[i] = nums[i]
     if (right + 1 - left >= 0) {
-      System.arraycopy(nums, left, temp, left, right + 1 - left);
+      System.arraycopy(nums, left, tmp, left, right + 1 - left);
     }
 
     int i = left;
     int j = mid + 1;
-
     int count = 0;
     for (int k = left; k <= right; k++) {
-
       if (i == mid + 1) {
-        nums[k] = temp[j];
+        nums[k] = tmp[j];
         j++;
       } else if (j == right + 1) {
-        nums[k] = temp[i];
+        nums[k] = tmp[i];
         i++;
-      } else if (temp[i] <= temp[j]) {
-        nums[k] = temp[i];
+      } else if (tmp[i] <= tmp[j]) {
+        nums[k] = tmp[i];
         i++;
       } else {
-        nums[k] = temp[j];
+        nums[k] = tmp[j];
         j++;
         count += (mid - i + 1);
       }
     }
     return count;
+  }
+
+  public static class BIT {
+    private final int[] tree;
+    private final int n;
+
+    public BIT(int n) {
+      this.n = n;
+      this.tree = new int[n + 1];
+    }
+
+    public static int lowBit(int x) {
+      return x & (-x);
+    }
+
+    public int query(int x) {
+      int ret = 0;
+      while (x != 0) {
+        ret += tree[x];
+        x -= lowBit(x);
+      }
+      return ret;
+    }
+
+    public void update(int x) {
+      while (x < n) {
+        ++tree[x];
+        x += lowBit(x);
+      }
+    }
   }
 
 }
