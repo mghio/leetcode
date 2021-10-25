a2f8395c8e5d6b1bc0fad75526a3bccdcc4b723f
mghio <mghio.dev@gmail.com>
2021-10-25 21:07:15 +0800

    feat(array): find that single one

---
+++ src/main/java/cn/mghio/leetcode/array/singleNumber.java
@@ -0,0 +1,34 @@
+package java.cn.mghio.leetcode.array;
+
+/**
+ * Given a non-empty array of integers nums, every element appears twice except for one. Find that
+ * single one.
+ *
+ * You must implement a solution with a linear runtime complexity and use only constant extra
+ * space.
+ *
+ * @author mghio
+ * @since 2021-10-25
+ */
+public class singleNumber {
+
+  /**
+   * Given a non-empty array of integers nums, find that single one.
+   *
+   * @param nums the array
+   * @return the single that one number
+   */
+  public int singleNumber(int[] nums) {
+    if (nums == null || nums.length < 1) {
+      return 0;
+    }
+
+    int result = 0;
+    for (int num : nums) {
+      result ^= num;
+    }
+
+    return result;
+  }
+
+}
