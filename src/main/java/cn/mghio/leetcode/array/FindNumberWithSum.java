add2a039450336d2cd590a77ee9d56518013c46c
mghio <mghio.dev@gmail.com>
2021-10-25 22:49:57 +0800

    feat(array): find number with target sum

---
+++ src/main/java/cn/mghio/leetcode/array/FindNumberWithSum.java
@@ -0,0 +1,36 @@
+package java.cn.mghio.leetcode.array;
+
+/**
+ * @author mghio
+ * @since 2021-10-25
+ */
+public class FindNumberWithSum {
+
+  /**
+   * Given an array, find two element sum equal target.
+   *
+   * @param nums the array
+   * @param target the target
+   * @return the index
+   */
+  public int[] findNumberWithSum(int[] nums, int target) {
+    if (nums == null || nums.length < 2) {
+      return nums;
+    }
+
+    int ahead = nums.length - 1;
+    int behind = 0;
+    while (ahead > behind) {
+      int curSum = nums[ahead] + nums[behind];
+      if (curSum == target) {
+        return new int[]{nums[behind], nums[ahead]};
+      } else if (curSum > target) {
+        ahead--;
+      } else {
+        behind++;
+      }
+    }
+    return new int[]{0, 0};
+  }
+
+}
