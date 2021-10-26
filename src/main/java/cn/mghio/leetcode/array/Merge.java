4c2592ae03b224a5dd05298e3d568a9a13814e87
mghio <mghio.dev@gmail.com>
2021-10-26 22:41:50 +0800

    feat(array): merge two sorted array solution 2

--- src/main/java/cn/mghio/leetcode/array/Merge.java
+++ src/main/java/cn/mghio/leetcode/array/Merge.java
@@ -29,7 +29,7 @@ public class Merge {
    * @param nums2 the other array
    * @param n the other array length
    */
-  public void merge(int[] nums1, int m, int[] nums2, int n) {
+  public void mergeSolution1(int[] nums1, int m, int[] nums2, int n) {
     if (nums1 == null || nums2 == null) {
       return;
     }
@@ -54,6 +54,33 @@ public class Merge {
     }
 
     System.arraycopy(ans, 0, nums1, 0, index);
+  }
+
+  /**
+   * Given two integer arrays, return sorted array.
+   *
+   * @param nums1 the array
+   * @param m the array length
+   * @param nums2 the other array
+   * @param n the other array length
+   */
+  public void mergeSolution2(int[] nums1, int m, int[] nums2, int n) {
+    int p1 = m - 1, p2 = n - 1;
+    int tail = m + n - 1;
+    int cur;
+
+    while (p1 >= 0 || p2 >= 0) {
+      if (p1 == -1) {
+        cur = nums2[p2--];
+      } else if (p2 == -1) {
+        cur = nums1[p1--];
+      } else if (nums1[p1] > nums2[p2]) {
+        cur = nums1[p1--];
+      } else {
+        cur = nums2[p2--];
+      }
+      nums1[tail--] = cur;
+    }
   }
 
 }
