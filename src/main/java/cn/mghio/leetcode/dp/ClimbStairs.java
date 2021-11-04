5e84aa97bec44a995c638d347a58264989f4e3ac
mghio <mghio.dev@gmail.com>
2021-11-04 22:42:43 +0800

    feat(dp): return how many distinct ways can you climb to the top solution 1

---
+++ src/main/java/cn/mghio/leetcode/dp/ClimbStairs.java
@@ -0,0 +1,35 @@
+package java.cn.mghio.leetcode.dp;
+
+/**
+ * You are climbing a staircase. It takes n steps to reach the top.
+ *
+ * Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+ *
+ * @author mghio
+ * @since 2021-11-04
+ */
+public class ClimbStairs {
+
+  /**
+   * Return how many distinct ways can you climb to the top
+   *
+   * @param n the n
+   * @return the result
+   */
+  public int climbStairsSolution1(int n) {
+    if (n == 0) {
+      return 0;
+    }
+
+    if (n == 1) {
+      return 1;
+    }
+
+    if (n == 2) {
+      return 2;
+    }
+
+    return climbStairsSolution1(n - 1) + climbStairsSolution1(n - 2);
+  }
+
+}
