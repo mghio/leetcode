0ade5348876b2ebb2e9b1a21b2ae75eb8bd05eb7
mghio <mghio.dev@gmail.com>
2021-11-08 23:43:03 +0800

    fix(dp): Return the maximum amount of money you can rob tonight without alerting the police

--- src/main/java/cn/mghio/leetcode/dp/Rob.java
+++ src/main/java/cn/mghio/leetcode/dp/Rob.java
@@ -34,7 +34,7 @@ public class Rob {
     dp[0] = nums[0];
     dp[1] = Math.max(nums[0], nums[1]);
     for (int i = 2; i < n; i++) {
-      dp[i] = Math.max(dp[i - 1], dp[i - 2] + dp[i]);
+      dp[i] = Math.max(dp[i - 1], dp[i - 2] + nums[i]);
     }
     return dp[n - 1];
   }
