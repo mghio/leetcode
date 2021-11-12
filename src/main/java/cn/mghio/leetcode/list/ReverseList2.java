cf4f7d9468c4a995ab80433ca5ec9dcfb3b6daf7
mghio <mghio.dev@gmail.com>
2021-11-12 22:54:18 +0800

    fix(single-list): Reverse single list

---
+++ src/main/java/cn/mghio/leetcode/list/ReverseList2.java
@@ -0,0 +1,41 @@
+package java.cn.mghio.leetcode.list;
+
+/**
+ * @author mghio
+ * @since 2021-11-12
+ */
+public class ReverseList2 {
+
+  /**
+   * given head of single list, reverse single list.
+   *
+   * @param head the head node
+   * @return the reversed list node
+   */
+  public ListNode reverseList(ListNode head) {
+    if (head == null || head.next == null) {
+      return head;
+    }
+
+    ListNode newNode = reverseList(head.next);
+    head.next.next = head;
+    head.next = null;
+    return newNode;
+  }
+
+  public class ListNode {
+
+    int val;
+    ListNode next;
+
+    public ListNode(int val) {
+      this.val = val;
+    }
+
+    public ListNode(int val, ListNode next) {
+      this.val = val;
+      this.next = next;
+    }
+  }
+
+}
