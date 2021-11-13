8f5199d398aea3acb21d8fea782e0b879d9597e1
mghio <mghio.dev@gmail.com>
2021-11-13 09:33:06 +0800

    fix(single-list): merge two list solution 2

--- src/main/java/cn/mghio/leetcode/list/MergeTwoList2.java
+++ src/main/java/cn/mghio/leetcode/list/MergeTwoList2.java
@@ -32,4 +32,18 @@ public class MergeTwoList2 {
     return dummy.next;
   }
 
+  public ListNode mergeTwoList2(ListNode l1, ListNode l2) {
+    if (l1 == null) {
+      return l2;
+    } else if (l2 == null) {
+      return l1;
+    } else if (l1.val < l2.val) {
+      l1.next = mergeTwoList2(l1.next, l2);
+      return l1;
+    } else {
+      l2.next = mergeTwoList(l1, l2.next);
+      return l2;
+    }
+  }
+
 }
