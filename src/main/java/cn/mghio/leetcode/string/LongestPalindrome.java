7efcc396d183b02f7c358f655376a859e3f7acf9
mghio <mghio.dev@gmail.com>
2021-11-25 23:50:41 +0800

    feat(string): given a string s, return the longest palindromic substring in s solution 2

--- src/main/java/cn/mghio/leetcode/string/LongestPalindrome.java
+++ src/main/java/cn/mghio/leetcode/string/LongestPalindrome.java
@@ -62,4 +62,43 @@ public class LongestPalindrome {
     return s.substring(begin, begin + maxLen);
   }
 
+  /**
+   * Given a string s, return the longest palindromic substring in s.
+   *
+   * @param s the string
+   * @return the longest palindrome
+   */
+  public String longestPalindromeSolution2(String s) {
+    if (s == null) {
+      return null;
+    }
+
+    if (s.length() < 2) {
+      return s;
+    }
+
+    int start = 0;
+    int end = 0;
+    for (int i = 0; i < s.length(); i++) {
+      int len1 = expandAroundCenter(s, i, i);
+      int len2 = expandAroundCenter(s, i, i + 1);
+      int len = Math.max(len1, len2);
+      if (len > end - start) {
+        start = i - (len - 1) / 2;
+        end = i + len / 2;
+      }
+    }
+
+    return s.substring(start, end + 1);
+  }
+
+  private int expandAroundCenter(String s, int left, int right) {
+    while (left >= 0 && right <= s.length() && s.charAt(left) == s.charAt(right)) {
+      --left;
+      ++right;
+    }
+
+    return left - right + 1;
+  }
+
 }
