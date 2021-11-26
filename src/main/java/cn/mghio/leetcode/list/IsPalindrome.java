2c6cdc23d30083222833269b4c348c00b8e03c1c
mghio <mghio.dev@gmail.com>
2021-11-26 23:47:55 +0800

    feat(list): return true if it is a palindrome solution 2

--- src/main/java/cn/mghio/leetcode/list/IsPalindrome.java
+++ src/main/java/cn/mghio/leetcode/list/IsPalindrome.java
@@ -21,6 +21,44 @@ public class IsPalindrome {
     return traverse(head);
   }
 
+  /**
+   * Return true if it is a palindrome.
+   *
+   * @param head the head node
+   * @return true if it is a palindrome, else false
+   */
+  public boolean isPalindromeSolution2(ListNode head) {
+    if (head == null) {
+      return true;
+    }
+
+    ListNode left = head;
+    ListNode slow = findToReverseHeadNode(head);
+    ListNode right = reverse(slow);
+
+    while (right != null) {
+      if (left.val != right.val) {
+        return false;
+      }
+      left = left.next;
+      right = right.next;
+    }
+
+    return true;
+  }
+
+  private ListNode reverse(ListNode head) {
+    ListNode pre = null;
+    ListNode cur = head;
+    while (cur != null) {
+      ListNode next = cur.next;
+      cur.next = pre;
+      pre = cur;
+      cur = next;
+    }
+    return pre;
+  }
+
   private boolean traverse(ListNode right) {
     if (right == null) {
       return true;
@@ -29,6 +67,27 @@ public class IsPalindrome {
     ans = ans && (right.val == left.val);
     left = left.next;
     return ans;
+  }
+
+  private ListNode findToReverseHeadNode(ListNode head) {
+    ListNode slow = head;
+    ListNode fast = head;
+    while (fast != null && fast.next != null) {
+      slow = slow.next;
+      fast = fast.next.next;
+    }
+
+    if (fast != null) {
+      slow = slow.next;
+    }
+    return slow;
+  }
+
+  public static void main(String[] args) {
+    ListNode node = new ListNode(1, new ListNode(2));
+    IsPalindrome palindrome = new IsPalindrome();
+    boolean ans = palindrome.isPalindromeSolution2(node);
+    System.out.println(ans);
   }
 
 }
