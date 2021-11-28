dc472a5615a324463454df7efc382116282388c3
mghio <mghio.dev@gmail.com>
2021-11-28 23:10:10 +0800

    feat(array): remove duplicates element of given array

---
+++ src/main/java/cn/mghio/leetcode/array/RemoveDuplicates.java
@@ -0,0 +1,36 @@
+package cn.mghio.leetcode.array;
+
+/**
+ * Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such
+ * that each unique element appears only once. The relative order of the elements should be kept the
+ * same.
+ *
+ * @author mghio
+ * @since 2021-11-28
+ */
+public class RemoveDuplicates {
+
+  /**
+   * Remove duplicates element of given array.
+   *
+   * @param nums the array
+   * @return the new length
+   */
+  public int removeDuplicates(int[] nums) {
+    if (nums == null || nums.length < 1) {
+      return 0;
+    }
+
+    int p = 0;
+    int q = 1;
+    while (q < nums.length) {
+      if (nums[p] != nums[q]) {
+        nums[++p] = nums[q];
+      }
+      q++;
+    }
+
+    return q + 1;
+  }
+
+}
