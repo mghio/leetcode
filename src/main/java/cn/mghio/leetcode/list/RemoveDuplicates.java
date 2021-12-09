34be86e0d10d57a9d7c15d5f6a375fe36f0294f5
mghio <mghio.dev@gmail.com>
2021-12-09 23:49:37 +0800

    feat(list): Given the head of a sorted linked list, delete all duplicates such that each element appears only once

---
+++ src/main/java/cn/mghio/leetcode/list/RemoveDuplicates.java
@@ -0,0 +1,37 @@
+package cn.mghio.leetcode.list;
+
+/**
+ * Given the head of a sorted linked list, delete all duplicates such that each element appears only
+ * once. Return the linked list sorted as well.
+ *
+ * @author mghio
+ * @since 2021-12-09
+ */
+public class RemoveDuplicates {
+
+  /**
+   * Given the head of a sorted linked list, delete all duplicates such that each element appears
+   * only once.
+   *
+   * @param head the head node
+   * @return the linked list sorted as well
+   */
+  public ListNode removeDuplicates(ListNode head) {
+    if (head == null) {
+      return null;
+    }
+
+    ListNode slow = head, fast = head;
+    while (fast != null) {
+      if (slow.val != fast.val) {
+        slow = slow.next;
+        slow.val = fast.val;
+      }
+      fast = fast.next;
+    }
+
+    slow.next = null;
+    return head;
+  }
+
+}
