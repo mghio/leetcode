c3aaf53d56a5ac8d36f21d7960a68d3c0b7ba65b
mghio <mghio.dev@gmail.com>
2021-12-20 23:41:06 +0800

    feat(dp): given two strings word1 and word2, return the minimum number of operations required to convert word1 to word2 solution 2

--- src/main/java/cn/mghio/leetcode/dp/MinDistance.java
+++ src/main/java/cn/mghio/leetcode/dp/MinDistance.java
@@ -43,6 +43,65 @@ public class MinDistance {
     return minDist;
   }
 
+  /**
+   * Given two strings word1 and word2, return the minimum number of operations required to convert
+   * word1 to word2.
+   *
+   * @param word1 the word1
+   * @param word2 the word2
+   * @return the min distance
+   */
+  public int minDistanceSolution2(String word1, String word2) {
+    if (word1 == null || word2 == null) {
+      return 0;
+    }
+    if (word1.length() == 0 || word2.length() == 0) {
+      return word1.length() == 0 ? word2.length() : word1.length();
+    }
+
+    return lwsDP(word1.toCharArray(), word1.length(), word2.toCharArray(), word2.length());
+  }
+
+  private int lwsDP(char[] a, int m, char[] b, int n) {
+    int[][] minDist = new int[m][n];
+
+    for (int i = 0; i < n; i++) {
+      if (a[0] == b[i]) {
+        minDist[0][i] = i;
+      } else if (i != 0) {
+        minDist[0][i] = minDist[0][i - 1] + 1;
+      } else {
+        minDist[0][i] = 1;
+      }
+    }
+
+    for (int i = 0; i < m; i++) {
+      if (a[i] == b[0]) {
+        minDist[i][0] = i;
+      } else if (i != 0) {
+        minDist[i][0] = minDist[i - 1][0] + 1;
+      } else {
+        minDist[i][0] = 1;
+      }
+    }
+
+    for (int i = 1; i < m; i++) {
+      for (int j = 1; j < n; j++) {
+        if (a[i] == b[j]) {
+          minDist[i][j] = min(minDist[i - 1][j] + 1, minDist[i][j - 1] + 1, minDist[i - 1][j - 1]);
+        } else {
+          minDist[i][j] = min(minDist[i - 1][j] + 1, minDist[i][j - 1] + 1, minDist[i - 1][j - 1] + 1);
+        }
+      }
+    }
+
+    return minDist[m - 1][n - 1];
+  }
+
+  private int min(int item1, int item2, int item3) {
+    return Math.min(Math.min(item1, item2), item3);
+  }
+
   private void lwsBT(int i, int j, int dist) {
     if (i == n || j == m) {
       if (i < n) {
