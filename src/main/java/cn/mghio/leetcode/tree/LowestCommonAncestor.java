d07a63939c1b8ed607ad8e3ae80587ca3afaf53e
mghio <mghio.dev@gmail.com>
2021-12-22 23:35:59 +0800

    feat(tree): given a binary tree, find the lowest common ancestor (LCA) of two given nodes in the tree solution 2.

--- src/main/java/cn/mghio/leetcode/tree/LowestCommonAncestor.java
+++ src/main/java/cn/mghio/leetcode/tree/LowestCommonAncestor.java
@@ -1,5 +1,10 @@
 package cn.mghio.leetcode.tree;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
 /**
  * Given a binary tree, find the lowest common ancestor (LCA) of two given nodes in the tree.
  *
@@ -9,6 +14,9 @@ package cn.mghio.leetcode.tree;
 public class LowestCommonAncestor {
 
   private TreeNode ans;
+
+  private final Map<Integer, TreeNode> parent = new HashMap<>();
+  private final Set<Integer> visited = new HashSet<>() ;
 
   /**
    * Given a binary tree, find the lowest common ancestor (LCA) of two given nodes in the tree.
@@ -25,6 +33,48 @@ public class LowestCommonAncestor {
 
     this.dfs(root, p, q);
     return ans;
+  }
+
+  /**
+   * Given a binary tree, find the lowest common ancestor (LCA) of two given nodes in the tree.
+   *
+   * @param root the root node
+   * @param p the p node
+   * @param q the q node
+   * @return the lowest common ancestor
+   */
+  public TreeNode lowestCommonAncestorSolution2(TreeNode root, TreeNode p, TreeNode q) {
+    if (root == null || p == null || q == null) {
+      return null;
+    }
+
+    dfs(root);
+
+    while (p != null) {
+      visited.add(p.val);
+      p = parent.get(p.val);
+    }
+
+    while (q != null) {
+      if (visited.contains(q.val)) {
+        return q;
+      }
+      q = parent.get(q.val);
+    }
+
+    return null;
+  }
+
+  private void dfs(TreeNode root) {
+    if (root.left != null) {
+      parent.put(root.left.val, root);
+      dfs(root);
+    }
+
+    if (root.right != null) {
+      parent.put(root.right.val, root);
+      dfs(root);
+    }
   }
 
   private boolean dfs(TreeNode root, TreeNode p, TreeNode q) {
