485212ff01d7e93fcdae6c26e9597d7ddc7a337b
mghio <mghio.dev@gmail.com>
2021-12-25 21:42:47 +0800

    feat(tree): given the root of a binary tree, invert the tree, and return its root solution 1

---
+++ src/main/java/cn/mghio/leetcode/tree/InvertTree.java
@@ -0,0 +1,31 @@
+package cn.mghio.leetcode.tree;
+
+/**
+ * Given the root of a binary tree, invert the tree, and return its root.
+ *
+ * @author mghio
+ * @since 2021-12-25
+ */
+public class InvertTree {
+
+  /**
+   * Given the root of a binary tree, invert the tree, and return its root.
+   *
+   * @param root the root node
+   * @return the inverted tree
+   */
+  public TreeNode invertTreeSolution1(TreeNode root) {
+    if (root == null) {
+      return null;
+    }
+
+    TreeNode tmp = root.left;
+    root.left = root.right;
+    root.right = tmp;
+
+    invertTreeSolution1(root.left);
+    invertTreeSolution1(root.right);
+    return root;
+  }
+
+}
