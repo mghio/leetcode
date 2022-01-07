9eb3fca9826b6ee0ff93d87458f97986efab57dd
mghio <mghio.dev@gmail.com>
2022-01-07 23:29:04 +0800

    refactor(dp): Given an unsorted array of integers nums, return the length of the longest continuous increasing subsequence solution 2.

--- src/main/java/cn/mghio/leetcode/dp/FindLengthOfLCIS.java
+++ src/main/java/cn/mghio/leetcode/dp/FindLengthOfLCIS.java
@@ -32,6 +32,31 @@ public class FindLengthOfLCIS {
     return _findMax(dp);
   }
 
+  /**
+   * Given an unsorted array of integers nums, return the length of the longest continuous
+   * increasing subsequence.
+   *
+   * @param nums the num array
+   * @return the length of longest
+   */
+  public int findLengthOfLCISSolution2(int[] nums) {
+    if (nums == null || nums.length == 0) {
+      return 0;
+    }
+
+    int res = 0;
+    int len = nums.length;
+    int start = 0;
+    for (int i = 0; i < len; i++) {
+      if (i > 0 && nums[i] > nums[i - 1]) {
+        start = i;
+      }
+      res = Math.max(res, i - start + 1);
+    }
+
+    return res;
+  }
+
   private int _findMax(int[] dp) {
     int res = 0;
     for (int len : dp) {
