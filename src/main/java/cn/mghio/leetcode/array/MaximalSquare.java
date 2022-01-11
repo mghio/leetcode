6b4351b003f407953539b11cb722d7ce825fc853
mghio <mghio.dev@gmail.com>
2022-01-12 00:18:52 +0800

    feat(dp): Given an m x n binary matrix filled with 0's and 1's, find the largest square containing only 1's and return its area solution 2.

--- src/main/java/cn/mghio/leetcode/array/MaximalSquare.java
+++ src/main/java/cn/mghio/leetcode/array/MaximalSquare.java
@@ -53,4 +53,37 @@ public class MaximalSquare {
     return maxSide * maxSide;
   }
 
+  /**
+   * Given an m x n binary matrix filled with 0's and 1's, find the largest square containing only 1's
+   * and return its area.
+   *
+   * @param matrix the array
+   * @return the result
+   */
+  public int maximalSquareSolution2(char[][] matrix) {
+    int maxSide = 0;
+    if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+      return 0;
+    }
+
+    int rowLen = matrix.length;
+    int colLen = matrix[0].length;
+    int[][] dp = new int[rowLen][colLen];
+
+    for (int i = 0; i < rowLen; i++) {
+      for (int j = 0; j < colLen; j++) {
+        if (matrix[i][j] == '1') {
+          if (i == 0 || j == 0) {
+            dp[i][j] = 1;
+          } else {
+            dp[i][j] = Math.min(Math.min(dp[i - 1][j], dp[i][j - 1]), dp[i - 1][j - 1]) + 1;
+          }
+          maxSide = Math.max(maxSide, dp[i][j]);
+        }
+      }
+    }
+
+    return maxSide * maxSide;
+  }
+
 }
