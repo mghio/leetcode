5a61e4998b8cf6ac240d4cc292abbd1e7da905cb
mghio <mghio.dev@gmail.com>
2022-01-12 08:52:12 +0800

    feat(tree): Given the root of a binary tree, return the maximum width of the given tree solution 1.

---
+++ src/main/java/cn/mghio/leetcode/tree/WidthOfBinaryTree.java
@@ -0,0 +1,56 @@
+package cn.mghio.leetcode.tree;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * Given the root of a binary tree, return the maximum width of the given tree.
+ *
+ * @author mghio
+ * @since 2022-01-12
+ */
+public class WidthOfBinaryTree {
+
+  /**
+   * Given the root of a binary tree, return the maximum width of the given tree.
+   *
+   * @param root the root node
+   * @return the result
+   */
+  public int withOfBinaryTreeSolution1(TreeNode root) {
+    Queue<Node> queue = new LinkedList<>();
+    queue.add(new Node(root, 0, 0));
+
+    int curDepth = 0;
+    int left = 0;
+    int res = 0;
+    while (!queue.isEmpty()) {
+      Node node = queue.poll();
+      if (node.node != null) {
+        queue.add(new Node(node.node.left, node.depth + 1, node.position * 2));
+        queue.add(new Node(node.node.right, node.depth + 1, node.position * 2 + 1));
+        if (curDepth != node.depth) {
+          curDepth = node.depth;
+          left = node.position;
+        }
+        res = Math.max(res, node.position - left + 1);
+      }
+    }
+
+    return res;
+  }
+
+  public static class Node {
+
+    TreeNode node;
+    int depth;
+    int position;
+
+    public Node(TreeNode node, int depth, int position) {
+      this.node = node;
+      this.depth = depth;
+      this.position = position;
+    }
+  }
+
+}
