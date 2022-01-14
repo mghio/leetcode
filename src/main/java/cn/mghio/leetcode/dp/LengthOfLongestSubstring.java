0a6cf893aecb6ab1de162e3e96444741d5cb131c
mghio <mghio.dev@gmail.com>
2022-01-14 08:23:04 +0800

    refactor(slidingwindow): Given two strings s and t of lengths m and n respectively, return the minimum window substring of s such that every character in t (including duplicates) is included in the window.

---
+++ src/main/java/cn/mghio/leetcode/dp/LengthOfLongestSubstring.java
@@ -0,0 +1,23 @@
+package cn.mghio.leetcode.dp;
+
+/**
+ * Given a string s, find the length of the longest substring without repeating characters.
+ *
+ * @author mghio
+ * @since 2022-01-13
+ */
+public class LengthOfLongestSubstring {
+
+  /**
+   * Given a string s, find the length of the longest substring without repeating characters.
+   *
+   * @param s the source string
+   * @return the length of the longest substring
+   */
+  public int lengthOfLongestSubstring(String s) {
+    
+
+    return 0;
+  }
+
+}
