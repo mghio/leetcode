d1b525e13228ebc2d6df2173584dd4f4bfac90de
mghio <mghio.dev@gmail.com>
2022-01-20 22:51:25 +0800

    feat(array): Given an array of integers nums and an integer k, return the total number of continuous subarrays whose sum equals to k solution 2.

--- src/main/java/cn/mghio/leetcode/array/SubarraySum.java
+++ src/main/java/cn/mghio/leetcode/array/SubarraySum.java
@@ -1,5 +1,8 @@
 package cn.mghio.leetcode.array;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Given an array of integers nums and an integer k, return the total number of continuous subarrays
  * whose sum equals to k.
@@ -32,11 +35,42 @@ public class SubarraySum {
     int res = 0;
     for (int i = 1; i <= len; i++) {
       for (int j = 0; j < i; j++) {
-        // nums[j ... i] = preSum[i] - preSum[j]
+        // nums[j ... i - 1] = preSum[i] - preSum[j]
         if (preSums[i] + preSums[j] == k) {
           res++;
         }
       }
+    }
+
+    return res;
+  }
+
+  /**
+   * Given an array of integers nums and an integer k, return the total number of continuous
+   * subarrays whose sum equals to k.
+   *
+   * @param nums the array
+   * @param k the sum
+   * @return the total number
+   */
+  public int subarraySumSolution2(int[] nums, int k) {
+    if (nums == null || nums.length == 0) {
+      return 0;
+    }
+
+    Map<Integer, Integer> preSumCountMap = new HashMap<>() {{
+      put(0, 1);
+    }};
+
+    int res = 0;
+    int sumI = 0;
+    for (int num : nums) {
+      sumI += num;
+      int sumJ = sumI - k;
+      if (preSumCountMap.containsKey(sumJ)) {
+        res += preSumCountMap.get(sumJ);
+      }
+      preSumCountMap.put(sumI, preSumCountMap.getOrDefault(sumI, 0) + 1);
     }
 
     return res;
