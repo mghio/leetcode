a43d2407367a9f1c404791cee8f77e1caadc51e7
mghio <mghio.dev@gmail.com>
2022-01-22 00:00:08 +0800

    feat(array): Return an array answer of length n, where answer[i] is the total number of seats reserved for flight i solution 2.

--- src/main/java/cn/mghio/leetcode/array/CorpFlightBookings.java
+++ src/main/java/cn/mghio/leetcode/array/CorpFlightBookings.java
@@ -87,21 +87,16 @@ public class CorpFlightBookings {
     int[] diff = new int[n];
 
     for (int[] booking : bookings) {
-      int i = booking[0] - 1;
-      int j = booking[1] - 1;
-      int val = booking[2];
-      diff[i] += val;
-      if (j + 1 < n) {
-        diff[j + 1] -= val;
+      diff[booking[0] - 1] += booking[2];
+      if (booking[1] < n) {
+        diff[booking[1]] -= booking[2];
       }
     }
 
-    int[] res = new int[n];
-    res[0] = diff[0];
-    for (int i = 1; i < diff.length; i++) {
-      res[i] = res[i - 1] + diff[i];
+    for (int i = 1; i < n; i++) {
+      diff[i] += diff[i - 1];
     }
-    return res;
+    return diff;
   }
 
 }
