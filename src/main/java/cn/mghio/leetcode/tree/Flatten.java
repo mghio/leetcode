7af13699265c490a225110195558273dd4043832
mghio <mghio.dev@gmail.com>
2022-01-23 10:28:47 +0800

    feat(tree): Given the root of a binary tree, flatten the tree into a "linked list" solution 2.

--- src/main/java/cn/mghio/leetcode/tree/Flatten.java
+++ src/main/java/cn/mghio/leetcode/tree/Flatten.java
@@ -1,5 +1,8 @@
 package cn.mghio.leetcode.tree;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Given the root of a binary tree, flatten the tree into a "linked list".
  *
@@ -13,13 +16,13 @@ public class Flatten {
    *
    * @param root the root node
    */
-  public void flatten(TreeNode root) {
+  public void flattenSolution1(TreeNode root) {
     if (root == null) {
       return;
     }
 
-    flatten(root.left);
-    flatten(root.right);
+    flattenSolution1(root.left);
+    flattenSolution1(root.right);
 
     TreeNode left = root.left;
     TreeNode right = root.right;
@@ -32,6 +35,37 @@ public class Flatten {
       p = p.right;
     }
     p.right = right;
+  }
+
+  private final List<TreeNode> preOrderNodes = new ArrayList<>();
+
+  /**
+   * Given the root of a binary tree, flatten the tree into a "linked list".
+   *
+   * @param root the root node
+   */
+  public void flattenSolution2(TreeNode root) {
+    if (root == null) {
+      return;
+    }
+
+    preOrderTraversal(root, preOrderNodes);
+
+    for (int i = 1, len = preOrderNodes.size(); i < len; i++) {
+      TreeNode pre = preOrderNodes.get(i - 1);
+      TreeNode cur = preOrderNodes.get(i);
+      pre.left = null;
+      pre.right = cur;
+    }
+  }
+
+  private void preOrderTraversal(TreeNode node, List<TreeNode> preOrderNodes) {
+    if (node != null) {
+      preOrderNodes.add(node);
+      preOrderTraversal(node.left, preOrderNodes);
+      preOrderTraversal(node.right, preOrderNodes);
+    }
+
   }
 
 }
