03a6dd27ce55d628392eb6c960ad70f8e32407fc
mghio <mghio.dev@gmail.com>
2022-01-23 11:25:53 +0800

    feat(tree): Given the root of a binary tree, return the preorder traversal of its nodes' values solution 3.

--- src/main/java/cn/mghio/leetcode/tree/PreorderTraversal.java
+++ src/main/java/cn/mghio/leetcode/tree/PreorderTraversal.java
@@ -1,6 +1,8 @@
 package cn.mghio.leetcode.tree;
 
 import java.util.ArrayList;
+import java.util.Deque;
+import java.util.LinkedList;
 import java.util.List;
 
 /**
@@ -21,12 +23,40 @@ public class PreorderTraversal {
    */
   public List<Integer> preorderTraversalSolution1(TreeNode root) {
     if (root == null) {
-      return null;
+      return res;
     }
 
     res.add(root.val);
     preorderTraversalSolution1(root.left);
     preorderTraversalSolution1(root.right);
+    return res;
+  }
+
+  /**
+   * Given the root of a binary tree, return the preorder traversal of its nodes' values.
+   *
+   * @param root the root node
+   * @return the preorder traversal of its nodes' values
+   */
+  public List<Integer> preorderTraversalSolution2(TreeNode root) {
+    List<Integer> res = new ArrayList<>();
+    if (root == null) {
+      return res;
+    }
+
+    Deque<TreeNode> stack = new LinkedList<>();
+    TreeNode node = root;
+    while (!stack.isEmpty() || node != null) {
+      while (node != null) {
+        res.add(node.val);
+        stack.push(node);
+        node = node.left;
+      }
+      node = stack.poll();
+      assert node != null;
+      node = node.right;
+    }
+
     return res;
   }
 
