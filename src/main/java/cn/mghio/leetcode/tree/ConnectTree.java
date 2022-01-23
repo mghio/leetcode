dd77d48cdd85abd2bc513e4e5019028973e9e229
mghio <mghio.dev@gmail.com>
2022-01-23 09:48:55 +0800

    feat(tree): You are given a perfect binary tree where all leaves are on the same level, and every parent has two children.

--- src/main/java/cn/mghio/leetcode/tree/ConnectTree.java
+++ src/main/java/cn/mghio/leetcode/tree/ConnectTree.java
@@ -1,5 +1,8 @@
 package cn.mghio.leetcode.tree;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * You are given a perfect binary tree where all leaves are on the same level, and every parent has
  * two children.
@@ -22,6 +25,41 @@ public class ConnectTree {
     }
 
     connectTwoNode(root.left, root.right);
+    return root;
+  }
+
+  /**
+   * You are given a perfect binary tree where all leaves are on the same level, and every parent has
+   * two children.
+   *
+   * @param root the root node
+   * @return the connected tree node
+   */
+  public TreeNode connectSolution2(TreeNode root) {
+    if (root == null) {
+      return null;
+    }
+
+    Queue<TreeNode> queue = new LinkedList<>();
+    queue.add(root);
+    while (!queue.isEmpty()) {
+      int levelSize = queue.size();
+      for (int i = 0; i < levelSize; i++) {
+        TreeNode node = queue.poll();
+        assert node != null;
+        if (i < levelSize - 1) {
+          node.next = queue.peek();
+        }
+
+        if (node.left != null) {
+          queue.add(node.left);
+        }
+        if (node.right != null) {
+          queue.add(node.right);
+        }
+      }
+    }
+
     return root;
   }
 
