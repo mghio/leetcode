5a1f672503c2d26d576d8e7aa811797063ff32cb
mghio <mghio.dev@gmail.com>
2022-01-26 22:58:50 +0800

    refactor(bst): implement a function to check if a binary tree is a binary search tree.

---
+++ src/main/java/cn/mghio/leetcode/bst/IsValidBST.java
@@ -0,0 +1,40 @@
+package cn.mghio.leetcode.bst;
+
+/**
+ * Implement a function to check if a binary tree is a binary search tree.
+ *
+ * @author mghio
+ * @since 2022-01-26
+ */
+public class IsValidBST {
+
+  /**
+   * Implement a function to check if a binary tree is a binary search tree.
+   *
+   * @param root the root node
+   * @return return true if a binary tree is a binary search tree, else false
+   */
+  public boolean isValidBST(TreeNode root) {
+    return isValidBST(root, null, null);
+  }
+
+  /**
+   * min.val < root.val < max.val
+   */
+  private boolean isValidBST(TreeNode root, TreeNode min, TreeNode max) {
+    if (root == null) {
+      return true;
+    }
+
+    if (min != null && root.val <= min.val) {
+      return false;
+    }
+
+    if (max != null && root.val >= max.val) {
+      return false;
+    }
+
+    return isValidBST(root.left, min, root) && isValidBST(root.right, root, max);
+  }
+
+}
