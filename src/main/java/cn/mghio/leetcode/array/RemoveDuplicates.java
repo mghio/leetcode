7db4080800e4a7df519eb60047696c6851b9bc73
mghio <mghio.dev@gmail.com>
2022-02-09 00:04:25 +0800

    feat(array): Remove duplicates element of given array solution 2.

--- src/main/java/cn/mghio/leetcode/array/RemoveDuplicates.java
+++ src/main/java/cn/mghio/leetcode/array/RemoveDuplicates.java
@@ -33,4 +33,27 @@ public class RemoveDuplicates {
     return q + 1;
   }
 
+  /**
+   * Remove duplicates element of given array.
+   *
+   * @param nums the array
+   * @return the new length
+   */
+  public int removeDuplicatesSolution2(int[] nums) {
+    if (nums == null || nums.length < 1) {
+      return 0;
+    }
+
+    int slow = 0;
+    int fast = 0;
+    while (fast < nums.length) {
+      if (nums[slow] != nums[fast]) {
+        nums[++slow] = nums[fast];
+      }
+      fast++;
+    }
+
+    return slow + 1;
+  }
+
 }
