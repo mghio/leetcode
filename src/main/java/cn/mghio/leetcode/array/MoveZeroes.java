5c1af2c958569510eca69ea5127b1ac17a9f6c6c
mghio <mghio.dev@gmail.com>
2022-02-09 22:07:50 +0800

    feat(array): Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero element solution 1.

---
+++ src/main/java/cn/mghio/leetcode/array/MoveZeroes.java
@@ -0,0 +1,45 @@
+package cn.mghio.leetcode.array;
+
+/**
+ * Given an integer array nums, move all 0's to the end of it while maintaining the relative order
+ * of the non-zero elements.
+ *
+ * Note that you must do this in-place without making a copy of the array.
+ *
+ * @author mghio
+ * @since 2022-02-09
+ */
+public class MoveZeroes {
+
+  /**
+   * Move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+   *
+   * @param nums the source array
+   */
+  public void moveZeroes(int[] nums) {
+    if (nums == null || nums.length == 0) {
+      return;
+    }
+
+    int noZeroElementIndex = removeElement(nums, 0);
+    for (int i = noZeroElementIndex; noZeroElementIndex < nums.length; i++) {
+      nums[i] = 0;
+    }
+  }
+
+  private int removeElement(int[] nums, int val) {
+    int slow = 0;
+    int fast = 0;
+
+    while (fast < nums.length) {
+      if (nums[fast] != val) {
+        nums[slow] = nums[fast];
+        slow++;
+      }
+      fast++;
+    }
+
+    return slow;
+  }
+
+}
