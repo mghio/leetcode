5f17ff16c9fe4b6ef93fb29b6084b7d25dd69f9c
mghio <mghio.dev@gmail.com>
2022-02-10 23:41:04 +0800

    refactor(backtrack): Given an array nums of distinct integers, return all the possible permutations. You can return the answer in any order solution 2.

--- src/main/java/cn/mghio/leetcode/backtracking/Permute.java
+++ src/main/java/cn/mghio/leetcode/backtracking/Permute.java
@@ -1,6 +1,8 @@
 package cn.mghio.leetcode.backtracking;
 
+import java.util.ArrayDeque;
 import java.util.ArrayList;
+import java.util.Deque;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -33,6 +35,8 @@ public class Permute {
 
   private void backtrack(int[] nums, LinkedList<Integer> track) {
     if (nums.length == track.size()) {
+      // In Java, parameter passing is by value, and in the process of passing parameters for
+      // object type variables, the address of the variable is copied.
       res.add(new ArrayList<>(track));
       return;
     }
@@ -46,6 +50,43 @@ public class Permute {
       track.add(num);
       backtrack(nums, track);
       track.removeLast();
+    }
+  }
+
+  /**
+   * Given an array nums of distinct integers, return all the possible permutations.
+   *
+   * @param nums the nums array
+   * @return all the possible permutations
+   */
+  public List<List<Integer>> permuteSolution2(int[] nums) {
+    if (nums == null || nums.length == 0) {
+      return res;
+    }
+
+    boolean[] used = new boolean[nums.length];
+    Deque<Integer> path = new ArrayDeque<>();
+    dfs(nums, nums.length, 0, path, used, res);
+    return res;
+  }
+
+  private void dfs(int[] nums, int length, int depth, Deque<Integer> path,
+      boolean[] used, List<List<Integer>> res) {
+    if (depth == length) {
+      res.add(new ArrayList<>(path));
+      return;
+    }
+
+    for (int i = 0; i < length; i++) {
+      if (used[i]) {
+        continue;
+      }
+
+      path.addLast(nums[i]);
+      used[i] = true;
+      dfs(nums, length, depth + 1, path, used, res);
+      used[i] = false;
+      path.removeLast();
     }
   }
 
