fc5c29e1560e47209a872afc531e4d9e1c504c00
mghio <mghio.dev@gmail.com>
2022-02-12 23:08:55 +0800

    feat(dp): Given a triangle array, return the minimum path sum from top to bottom solution 1.

---
+++ src/main/java/cn/mghio/leetcode/dp/MinimumTotal.java
@@ -0,0 +1,43 @@
+package cn.mghio.leetcode.dp;
+
+import java.util.List;
+
+/**
+ * Given a triangle array, return the minimum path sum from top to bottom.
+ *
+ * For each step, you may move to an adjacent number of the row below. More formally, if you are on
+ * index i on the current row, you may move to either index i or index i + 1 on the next row.
+ *
+ * @author mghio
+ * @since 2022-02-12
+ */
+public class MinimumTotal {
+
+  /**
+   * Given a triangle array, return the minimum path sum from top to bottom.
+   *
+   * @param triangle the triangle
+   * @return the minimum total
+   */
+  public int minimumTotal(List<List<Integer>> triangle) {
+    if (triangle == null || triangle.size() == 0) {
+      return 0;
+    }
+
+    int m = triangle.size();
+    int[][] dp = new int[m][];
+    List<Integer> lastRow = triangle.get(m - 1);
+    for (int i = 0; i < lastRow.size(); i++) {
+      dp[m - 1][i] = lastRow.get(i);
+    }
+
+    for (int i = triangle.size() - 2; i >= 0; i--) {
+      for (int j = 0; j < triangle.get(i).size(); j++) {
+        dp[i][j] = Math.min(dp[i + 1][j], dp[i + 1][j + 1]) + triangle.get(i).get(j);
+      }
+    }
+
+    return dp[0][0];
+  }
+
+}
