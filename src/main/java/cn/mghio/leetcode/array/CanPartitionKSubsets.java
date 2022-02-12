58e694797750a0f73d75fd5c4ab6b247cb6c0a9e
mghio <mghio.dev@gmail.com>
2022-02-12 15:57:51 +0800

    feat(backtrack): Return true if it is possible to divide this array into non-empty subsets whose sums are all equal solution 2.

--- src/main/java/cn/mghio/leetcode/array/CanPartitionKSubsets.java
+++ src/main/java/cn/mghio/leetcode/array/CanPartitionKSubsets.java
@@ -60,4 +60,57 @@ public class CanPartitionKSubsets {
     return false;
   }
 
+  /**
+   * Return true if it is possible to divide this array into non-empty subsets whose sums are all
+   * equal
+   */
+  public boolean canPartitionKSubsetsSolution2(int[] nums, int k) {
+    if (nums == null || nums.length == 0) {
+      return false;
+    }
+    if (nums.length < k) {
+      return false;
+    }
+    int sum = 0;
+    for (int num : nums) {
+      sum += num;
+    }
+    if (sum % k != 0) {
+      return false;
+    }
+
+    int targetSum = sum / k;
+    boolean[] used = new boolean[nums.length];
+    return backtrack(k, 0, nums, 0, used, targetSum);
+  }
+
+  private boolean backtrack(int k, int bucket, int[] nums, int start, boolean[] used, int target) {
+    if (k == 0) {
+      return true;
+    }
+
+    if (bucket == target) {
+      return backtrack(k - 1, 0, nums, 0, used, target);
+    }
+
+    for (int i = start; i < nums.length; i++) {
+      if (used[i]) {
+        continue;
+      }
+      if (bucket + nums[i] > target) {
+        continue;
+      }
+
+      used[i] = true;
+      bucket += nums[i];
+      if (backtrack(k, bucket, nums, i + 1, used, target)) {
+        return true;
+      }
+      used[i] = false;
+      bucket -= nums[i];
+    }
+
+    return false;
+  }
+
 }
