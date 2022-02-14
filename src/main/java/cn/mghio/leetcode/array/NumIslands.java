7a96c877dbc81fc638404d0de6f8bbeefffa805f
mghio <mghio.dev@gmail.com>
2022-02-14 23:35:39 +0800

    feat(dp): Given an m x n 2D binary grid grid which represents a map of '1's (land) and '0's (water), return the number of islands solution 2.

--- src/main/java/cn/mghio/leetcode/array/NumIslands.java
+++ src/main/java/cn/mghio/leetcode/array/NumIslands.java
@@ -40,6 +40,34 @@ public class NumIslands {
     return res;
   }
 
+  private final int[][] dirs = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+
+  /**
+   * Given an m x n 2D binary grid grid which represents a map of '1's (land) and '0's (water),
+   * return the number of islands.
+   *
+   * @param grid the grid
+   * @return the result
+   */
+  public int numIsLandsSolution2(char[][] grid) {
+    if (grid == null || grid.length == 0) {
+      return 0;
+    }
+
+    int res = 0;
+    int m = grid.length;
+    int n = grid[0].length;
+    for (int i = 0; i < m; i++) {
+      for (int j = 0; j < n; j++) {
+        if (grid[i][j] == '1') {
+          res++;
+          dfs(grid, i, j);
+        }
+      }
+    }
+    return res;
+  }
+
   private void dfs(char[][] grid, int i, int j) {
     int m = grid.length;
     int n = grid[0].length;
@@ -52,10 +80,9 @@ public class NumIslands {
     }
 
     grid[i][j] = '0';
-    dfs(grid, i + 1, j);
-    dfs(grid, i, j + 1);
-    dfs(grid, i, j - 1);
-    dfs(grid, i - 1, j);
+    for (int[] dir : dirs) {
+      dfs(grid, i + dir[0], j + dir[1]);
+    }
   }
 
 }
