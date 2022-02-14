d19ed6a61436fdd9a79c3dbc37315629c3b35a9f
mghio <mghio.dev@gmail.com>
2022-02-14 23:01:35 +0800

    refactor(dp): Given a triangle array, return the minimum path sum from top to bottom solution 4.

--- src/main/java/cn/mghio/leetcode/dp/MinimumTotal.java
+++ src/main/java/cn/mghio/leetcode/dp/MinimumTotal.java
@@ -92,8 +92,31 @@ public class MinimumTotal {
       return memo[i][j];
     }
 
-    memo[i][j] = Math.min(dfs(triangle, i + 1, j), dfs(triangle, i + 1, j + 1)) + triangle.get(i).get(j);
+    memo[i][j] =
+        Math.min(dfs(triangle, i + 1, j), dfs(triangle, i + 1, j + 1)) + triangle.get(i).get(j);
     return memo[i][j];
+  }
+
+  /**
+   * Given a triangle array, return the minimum path sum from top to bottom.
+   *
+   * @param triangle the triangle
+   * @return the minimum total
+   */
+  public int minimumTotalSolution4(List<List<Integer>> triangle) {
+    if (triangle == null || triangle.size() == 0) {
+      return 0;
+    }
+
+    int n = triangle.size();
+    int[] dp = new int[n + 1];
+    for (int i = n - 1; i >= 0; i--) {
+      for (int j = 0; j <= i; j++) {
+        dp[j] = Math.min(dp[j], dp[j + 1]) + triangle.get(i).get(j);
+      }
+    }
+
+    return dp[0];
   }
 
 }
