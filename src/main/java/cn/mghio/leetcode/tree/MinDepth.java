63fa7ea95ae9b69e7ca52814e38b64ffcbc540b4
mghio <mghio.dev@gmail.com>
2022-02-16 21:55:38 +0800

    feat(tree): Given a binary tree, find its minimum depth solution 1.

---
+++ src/main/java/cn/mghio/leetcode/tree/MinDepth.java
@@ -0,0 +1,51 @@
+package cn.mghio.leetcode.tree;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * Given a binary tree, find its minimum depth.
+ *
+ * @author mghio
+ * @since 2022-02-16
+ */
+public class MinDepth {
+
+  /**
+   * Given a binary tree, find its minimum depth.
+   *
+   * @param root the root node
+   * @return the minimum depth
+   */
+  public int minDepthUseBFS(TreeNode root) {
+    if (root == null) {
+      return 0;
+    }
+
+    Queue<TreeNode> queue = new LinkedList<>();
+    queue.add(root);
+    int depth = 1;
+
+    while (!queue.isEmpty()) {
+      int size = queue.size();
+      for (int i = 0; i < size; i++) {
+        TreeNode node = queue.poll();
+        assert node != null;
+        if (node.left == null && node.right == null) {
+          return depth;
+        }
+
+        if (node.left != null) {
+          queue.offer(node.left);
+        }
+
+        if (node.right != null) {
+          queue.offer(node.right);
+        }
+      }
+      depth++;
+    }
+    return depth;
+  }
+
+}
