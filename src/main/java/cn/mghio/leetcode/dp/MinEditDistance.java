f729bbcc45b470e76a1579b9e0089c82656cca47
mghio <mghio.dev@gmail.com>
2022-02-22 22:56:38 +0800

    feat(dp): Given two strings word1 and word2, return the minimum number of operations required to convert word1 to word2 solution 2.

--- src/main/java/cn/mghio/leetcode/dp/MinEditDistance.java
+++ src/main/java/cn/mghio/leetcode/dp/MinEditDistance.java
@@ -65,4 +65,45 @@ public class MinEditDistance {
     return Math.min(dp[i - 1][j] + 1, Math.min(dp[i][j - 1] + 1, dp[i - 1][j - 1] + 1));
   }
 
+  /**
+   * Given two strings word1 and word2, return the minimum number of operations required to convert
+   * word1 to word2.
+   *
+   * @param word1 the word
+   * @param word2 the other word
+   * @return the minimum distance
+   */
+  public int minEditDistSolution2(String word1, String word2) {
+    int m = word1.length();
+    int n = word2.length();
+    if (m == 0 || n == 0) {
+      return m + n;
+    }
+
+    int[][] dp = new int[m + 1][n + 1];
+
+    for (int i = 0; i < m + 1; i++) {
+      dp[i][0] = i;
+    }
+    for (int j = 0; j < n + 1; j++) {
+      dp[0][j] = j;
+    }
+
+    for (int i = 1; i < m + 1; i++) {
+      for (int j = 1; j < n + 1; j++) {
+        int left = dp[i - 1][j] + 1;
+        int down = dp[i][j - 1] + 1;
+        int leftDown = dp[i - 1][j - 1];
+        char c1 = word1.charAt(i - 1);
+        char c2 = word2.charAt(j - 1);
+        if (c1 != c2) {
+          leftDown += 1;
+        }
+        dp[i][j] = Math.min(left, Math.min(down, leftDown));
+      }
+    }
+
+    return dp[m][n];
+  }
+
 }
