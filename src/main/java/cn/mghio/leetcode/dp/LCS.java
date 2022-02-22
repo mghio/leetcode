55fcb2aa6a1c6f52c484b8731a34952a4d65a1c8
mghio <mghio.dev@gmail.com>
2022-02-22 08:45:20 +0800

    feat(dp): Given two strings text1 and text2, return the length of their longest common subsequence. If there is no common subsequence, return 0 solution 2.

--- src/main/java/cn/mghio/leetcode/dp/LCS.java
+++ src/main/java/cn/mghio/leetcode/dp/LCS.java
@@ -52,4 +52,36 @@ public class LCS {
     return memo[i][j];
   }
 
+  /**
+   * Given two strings text1 and text2, return the length of their longest common subsequence. If
+   * there is no common subsequence, return 0.
+   *
+   * @param text1 the string
+   * @param text2 the other string
+   * @return the common subsequence length
+   */
+  public int longestCommonSubsequenceSolution2(String text1, String text2) {
+    if (text1 == null || text1.length() == 0 || text2 == null || text2.length() == 0) {
+      return 0;
+    }
+
+    int m = text1.length();
+    int n = text2.length();
+    int[][] dp = new int[m + 1][n + 1];
+
+    for (int i = 1; i <= m; i++) {
+      char c1 = text1.charAt(i - 1);
+      for (int j = 1; j <= n; j++) {
+        char c2 = text2.charAt(j - 1);
+        if (c1 == c2) {
+          dp[i][j] = dp[i - 1][j - 1] + 1;
+        } else {
+          dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+        }
+      }
+    }
+
+    return dp[m][n];
+  }
+
 }
