2028c3aa0723d9b99d9b3b7b38de9f4aa2123015
mghio <mghio.dev@gmail.com>
2022-02-24 23:50:45 +0800

    refactor(array): Given an array of meeting time intervals where intervals[i] = [starti, endi], determine if a person could attend all meetings solution 2.

--- src/main/java/cn/mghio/leetcode/array/CanAttendMeetings.java
+++ src/main/java/cn/mghio/leetcode/array/CanAttendMeetings.java
@@ -45,8 +45,7 @@ public class CanAttendMeetings {
   }
 
   private boolean overlap(int[] interval1, int[] interval2) {
-    return (interval1[0] >= interval2[0] && interval1[0] < interval2[1])
-        || (interval2[0] >= interval1[0] && interval2[0] < interval1[1]);
+    return Math.min(interval1[1], interval2[1]) > Math.max(interval1[0], interval2[0]);
   }
 
 }
