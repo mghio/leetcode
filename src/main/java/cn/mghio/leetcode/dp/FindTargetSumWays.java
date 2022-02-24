c82cb5d1cb11145795b73a1acb18dcda2a9d7995
mghio <mghio.dev@gmail.com>
2022-02-24 22:48:15 +0800

    refactor(dp): You are given an integer array nums and an integer target solution 2.

--- src/main/java/cn/mghio/leetcode/dp/FindTargetSumWays.java
+++ src/main/java/cn/mghio/leetcode/dp/FindTargetSumWays.java
@@ -42,18 +42,14 @@ public class FindTargetSumWays {
 
     int n = nums.length;
     int neg = diff / 2;
-    int[][] dp = new int[n + 1][neg + 1];
-    dp[0][0] = 1;
-    for (int i = 1; i <= n; i++) {
-      int num = nums[i - 1];
-      for (int j = 0; j <= neg; j++) {
-        dp[i][j] = dp[i - 1][j];
-        if (j >= num) {
-          dp[i][j] += dp[i - 1][j - num];
-        }
+    int[] dp = new int[neg + 1];
+    dp[0] = 1;
+    for (int num : nums) {
+      for (int j = neg; j >= num; j--) {
+        dp[j] += dp[j - num];
       }
     }
-    return dp[n][neg];
+    return dp[neg];
   }
 
 }
