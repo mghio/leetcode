e021190ba0912ce0f4cf724b0605dc0f369f8f1e
mghio <mghio.dev@gmail.com>
2022-02-26 23:31:30 +0800

    feat(array): corp flight bookings.

--- src/main/java/cn/mghio/leetcode/array/CorpFlightBookings.java
+++ src/main/java/cn/mghio/leetcode/array/CorpFlightBookings.java
@@ -84,19 +84,28 @@ public class CorpFlightBookings {
    * @return the total number of seats reserved for flight i
    */
   public int[] corpFlightBookingsSolution2(int[][] bookings, int n) {
-    int[] diff = new int[n];
+    int[] res = new int[n];
+    if (bookings == null || bookings.length == 0 || n <= 0) {
+      return res;
+    }
 
+    int[] diff = new int[n];
     for (int[] booking : bookings) {
-      diff[booking[0] - 1] += booking[2];
-      if (booking[1] < n) {
-        diff[booking[1]] -= booking[2];
+      int i = booking[0] - 1;
+      int j = booking[1] - 1;
+      int val = booking[2];
+      diff[i] += val;
+      if (j + 1 < n) {
+        diff[j + 1] -= val;
       }
     }
 
-    for (int i = 1; i < n; i++) {
-      diff[i] += diff[i - 1];
+    res[0] = diff[0];
+    for (int i = 1; i < diff.length; i++) {
+      res[i] = res[i - 1] + diff[i];
     }
-    return diff;
+
+    return res;
   }
 
 }
