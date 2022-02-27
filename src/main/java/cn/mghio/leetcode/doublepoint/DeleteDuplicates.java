a292b2168dbe98fd526544715eefd51f1da03722
mghio <mghio.dev@gmail.com>
2022-02-27 12:59:23 +0800

    feat(doublepoint): Given the head of a sorted linked list, delete all duplicates such that each element appears only once. Return the linked list sorted as well.--continue

--- src/main/java/cn/mghio/leetcode/doublepoint/DeleteDuplicates.java
+++ src/main/java/cn/mghio/leetcode/doublepoint/DeleteDuplicates.java
@@ -28,4 +28,22 @@ public class DeleteDuplicates {
     return head;
   }
 
+
+  public ListNode deleteDuplicatesSolution2(ListNode head) {
+    if (head == null) {
+      return null;
+    }
+
+    ListNode curr = head;
+    while (curr.next != null) {
+      if (curr.val == curr.next.val) {
+        curr.next = curr.next.next;
+      } else {
+        curr = curr.next;
+      }
+    }
+
+    return head;
+  }
+
 }
