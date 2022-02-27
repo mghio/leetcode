b4c804bc460315b069ecfdac3f9f737363b61086
mghio <mghio.dev@gmail.com>
2022-02-27 23:20:52 +0800

    feat(array): Given an array and target, return two index of sum equals target solution 2.

--- src/main/java/cn/mghio/leetcode/array/TwoSum.java
+++ src/main/java/cn/mghio/leetcode/array/TwoSum.java
@@ -32,4 +32,27 @@ public class TwoSum {
     return new int[]{-1, -1};
   }
 
+  /**
+   * Given an array and target, return two index of sum equals target.
+   *
+   * @param nums the array
+   * @param target the target
+   * @return the index array
+   */
+  public int[] twoSumSolution2(int[] nums, int target) {
+    if (nums == null || nums.length == 0) {
+      return new int[]{-1, -1};
+    }
+
+    int len = nums.length;
+    for (int i = 0; i < len; i++) {
+      for (int j = i + 1; j < len; j++) {
+        if (nums[j] == target - nums[i]) {
+          return new int[]{i, j};
+        }
+      }
+    }
+    return new int[]{-1, -1};
+  }
+
 }
