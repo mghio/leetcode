4525d06acb1393bc616088d31c490c6e6033509b
mghio <43461943+mghio@users.noreply.github.com>
2022-03-01 07:59:11 +0800

    feat(list):Merge the two lists in a one sorted list. The list should be made by splicing together the nodes of the first two lists.

--- src/main/java/cn/mghio/leetcode/list/MergeTwoList.java
+++ src/main/java/cn/mghio/leetcode/list/MergeTwoList.java
@@ -24,15 +24,16 @@ public class MergeTwoList {
   public ListNode mergeTwoList(ListNode l1, ListNode l2) {
     ListNode dummy = new ListNode(-1);
     ListNode p = dummy;
-    ListNode p1 = l1, p2 = l2;
+    ListNode p1 = l1;
+    ListNode p2 = l2;
 
     while (p1 != null && p2 != null) {
-      if (p1.val > p2.val) {
-        p.next = p2;
-        p2 = p2.next;
-      } else {
+      if (p1.val < p2.val) {
         p.next = p1;
         p1 = p1.next;
+      } else {
+        p.next = p2;
+        p2 = p2.next;
       }
       p = p.next;
     }
