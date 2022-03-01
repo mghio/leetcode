176e6781117be336ee6234971bc5990182aea344
mghio <43461943+mghio@users.noreply.github.com>
2022-03-01 08:45:14 +0800

    feat(list): Input a linked list, and output the kth node from the bottom of the linked list.

--- src/main/java/cn/mghio/leetcode/list/GetKthFromEnd.java
+++ src/main/java/cn/mghio/leetcode/list/GetKthFromEnd.java
@@ -13,21 +13,17 @@ public class GetKthFromEnd {
   /**
    * Input a linked list, and output the kth node from the bottom of the linked list.
    *
-   * @param node the first node of linked list
+   * @param head the first node of linked list
    * @param k the k end
    * @return the kth node
    */
-  public SinglyNode getKthFromEnd(SinglyNode node, int k) {
-    if (node == null) {
-      return null;
-    }
-
-    SinglyNode p1 = node;
+  public ListNode getKthFromEnd(ListNode head, int k) {
+    ListNode p1 = head;
     for (int i = 0; i < k; i++) {
       p1 = p1.next;
     }
 
-    SinglyNode p2 = node;
+    ListNode p2 = head;
     while (p1 != null) {
       p2 = p2.next;
       p1 = p1.next;
