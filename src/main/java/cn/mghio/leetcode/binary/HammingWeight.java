68efd42a45cba80765edf66c5e5d3f15d558cf63
mghio <43461943+mghio@users.noreply.github.com>
2022-03-03 22:55:51 +0800

    feat(binary): Write a function that takes an unsigned integer and returns the number of '1' bits it has (also known as the Hamming weight).

---
+++ src/main/java/cn/mghio/leetcode/binary/HammingWeight.java
@@ -0,0 +1,22 @@
+package cn.mghio.leetcode.binary;
+
+/**
+ * Write a function that takes an unsigned integer and returns the number of '1' bits it has (also
+ * known as the Hamming weight).
+ *
+ * @author mghio
+ * @since 2022-03-03
+ */
+public class HammingWeight {
+
+  public int hammingWeight(int n) {
+    int res = 0;
+    for (int i = 0; i < 32; i++) {
+      if ((n & (1 << i)) != 0) {
+        res++;
+      }
+    }
+    return res;
+  }
+
+}
