638ce608c86a8487c67e1d9032d4ba79b37cf3c4
mghio <43461943+mghio@users.noreply.github.com>
2022-03-07 21:23:21 +0800

    feat(list): Given the head of a linked list, remove the nth node from the end of the list and return its head solution 2.

--- src/main/java/cn/mghio/leetcode/list/RemoveNthFromEnd.java
+++ src/main/java/cn/mghio/leetcode/list/RemoveNthFromEnd.java
@@ -33,4 +33,31 @@ public class RemoveNthFromEnd {
     return dummy.next;
   }
 
+  /**
+   * remove the nth node from the end of the list and return its.
+   *
+   * @param head the head of singly-linked list.
+   * @param n the n th
+   * @return the nth node from the end of list
+   */
+  public SinglyNode removeNthFromEndSolution2(SinglyNode head, int n) {
+    SinglyNode dummy = new SinglyNode(-1, head);
+    int length = getLength(head);
+    SinglyNode curr = dummy;
+    for (int i = 1; i < length - n + 1; i++) {
+      curr = curr.next;
+    }
+    curr.next = curr.next.next;
+    return dummy.next;
+  }
+
+  private int getLength(SinglyNode head) {
+    int length = 0;
+    while (head != null) {
+      length++;
+      head = head.next;
+    }
+    return length;
+  }
+
 }
