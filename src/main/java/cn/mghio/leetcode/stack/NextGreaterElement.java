cbd7fb6c5902369be3e85884f31a19ddde02c692
mghio <43461943+mghio@users.noreply.github.com>
2022-03-10 23:21:08 +0800

    feat(stack): The next greater element of some element x in an array is the first greater element that is to the right of x in the same array solution 3.

--- src/main/java/cn/mghio/leetcode/stack/NextGreaterElement.java
+++ src/main/java/cn/mghio/leetcode/stack/NextGreaterElement.java
@@ -1,5 +1,7 @@
 package cn.mghio.leetcode.stack;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -47,7 +49,6 @@ public class NextGreaterElement {
     return res;
   }
 
-
   public int[] nextGreaterElementSolution2(int[] nums1, int[] nums2) {
     if (nums1 == null || nums1.length == 0 || nums2 == null || nums2.length == 0) {
       return new int[]{};
@@ -71,6 +72,30 @@ public class NextGreaterElement {
       }
 
       res[i] = k < lenOfNums2 ? nums2[k] : -1;
+    }
+
+    return res;
+  }
+
+  public int[] nextGreaterElementSolution3(int[] nums1, int[] nums2) {
+    if (nums1 == null || nums1.length == 0 || nums2 == null || nums2.length == 0) {
+      return new int[]{};
+    }
+
+    Map<Integer, Integer> map = new HashMap<>();
+    Deque<Integer> stack = new ArrayDeque<>();
+    for (int i = nums2.length - 1; i >= 0; i--) {
+      int num = nums2[i];
+      while (!stack.isEmpty() && num >= stack.peek()) {
+        stack.pop();
+      }
+      map.put(num, stack.isEmpty() ? -1 : stack.peek());
+      stack.push(num);
+    }
+
+    int[] res = new int[nums1.length];
+    for (int i = 0; i < nums1.length; i++) {
+      res[i] = map.get(nums1[i]);
     }
 
     return res;
