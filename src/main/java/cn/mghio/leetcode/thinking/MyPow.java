830ccc55a8f3ba5c996ced9d5e36cdd3f648b33a
mghio <43461943+mghio@users.noreply.github.com>
2022-03-14 09:19:21 +0800

    feat(thinking): Implement pow(x, n), which calculates x raised to the power n (i.e., xn) solution 2.

--- src/main/java/cn/mghio/leetcode/thinking/MyPow.java
+++ src/main/java/cn/mghio/leetcode/thinking/MyPow.java
@@ -22,4 +22,18 @@ public class MyPow {
     return n > 0 ? ret : 1.0 / ret;
   }
 
+  public double pow2(double x, int n) {
+    long N = n;
+    return N >= 0 ? quickMul(x, N) : 1.0 / quickMul(x, -N);
+  }
+
+  private double quickMul(double x, long N) {
+    if (N == 0) {
+      return 1.0;
+    }
+
+    double y = quickMul(x, N / 2);
+    return N % 2 == 0 ? y * y : y * y * x;
+  }
+
 }
