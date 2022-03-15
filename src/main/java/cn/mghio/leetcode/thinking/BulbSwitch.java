981b170fc209487d3215e7f3194bcc138852ba28
mghio <43461943+mghio@users.noreply.github.com>
2022-03-15 23:15:24 +0800

    feat(thinking): bulb switch.

---
+++ src/main/java/cn/mghio/leetcode/thinking/BulbSwitch.java
@@ -0,0 +1,18 @@
+package cn.mghio.leetcode.thinking;
+
+/**
+ * There are n bulbs that are initially off. You first turn on all the bulbs, then you turn off
+ * every second bulb. On the third round, you toggle every third bulb (turning on if it's off or
+ * turning off if it's on). For the ith round, you toggle every i bulb. For the nth round, you only
+ * toggle the last bulb. Return the number of bulbs that are on after n rounds.
+ *
+ * @author mghio
+ * @since 2022-03-15
+ */
+public class BulbSwitch {
+
+  public int bulbSwitch(int n) {
+    return (int) Math.sqrt(n);
+  }
+
+}
