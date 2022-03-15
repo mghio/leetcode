e533bb9aa84c9231a631d60115a77fcad19433f8
mghio <43461943+mghio@users.noreply.github.com>
2022-03-15 23:08:55 +0800

    feat(thinking): The objective of the game is to end with the most stones. The total number of stones across all the piles is odd, so there are no ties.

---
+++ src/main/java/cn/mghio/leetcode/thinking/StoneGame.java
@@ -0,0 +1,19 @@
+package cn.mghio.leetcode.thinking;
+
+/**
+ * Alice and Bob play a game with piles of stones. There are an even number of piles arranged in a
+ * row, and each pile has a positive integer number of stones piles[i].
+ *
+ * The objective of the game is to end with the most stones. The total number of stones across all
+ * the piles is odd, so there are no ties.
+ *
+ * @author mghio
+ * @since 2022-03-15
+ */
+public class StoneGame {
+
+  public boolean stoneGame(int[] piles) {
+    return true;
+  }
+
+}
