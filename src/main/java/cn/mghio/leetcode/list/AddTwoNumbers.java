73086ecf11e84aa3cac2a9bb2bfb84eff0295849
mghio <43461943+mghio@users.noreply.github.com>
2022-03-20 23:34:26 +0800

    fix(list): Add Two Numbers.

---
+++ src/main/java/cn/mghio/leetcode/list/AddTwoNumbers.java
@@ -0,0 +1,87 @@
+package cn.mghio.leetcode.list;
+
+/**
+ * Add Two Numbers.
+ *
+ * You are given two non-empty linked lists representing two non-negative integers. The digits are
+ * stored in reverse order, and each of their nodes contains a single digit. Add the two numbers and
+ * return the sum as a linked list.
+ *
+ * @author mghio
+ * @since 2022-03-20
+ */
+public class AddTwoNumbers {
+
+  public static void main(String[] args) {
+    ListNode node13 = new ListNode(3);
+    ListNode node12 = new ListNode(4, node13);
+    ListNode head1 = new ListNode(2, node12);
+
+    ListNode node23 = new ListNode(4);
+    ListNode node22 = new ListNode(6, node23);
+    ListNode head2 = new ListNode(5, node22);
+
+    AddTwoNumbers addTwoNumbers = new AddTwoNumbers();
+    addTwoNumbers.addTwoNumbers(head1, head2);
+  }
+
+  public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+    if (l1 == null || l2 == null) {
+      return l1 == null ? l2 : l1;
+    }
+
+    ListNode reverseL1 = reverse(l1);
+    ListNode reverseL2 = reverse(l2);
+
+    ListNode dummy = new ListNode(-1);
+    ListNode p = dummy;
+    int carry = 0;
+    while (reverseL1 != null || reverseL2 != null) {
+      int val;
+      int curCarry = 0;
+      if (reverseL1 != null && reverseL2 != null) {
+        val = reverseL1.val + reverseL2.val;
+        if (val >= 10) {
+          curCarry = 1;
+          val = val % 10;
+        }
+      } else if (reverseL1 != null) {
+        val = reverseL1.val;
+      } else {
+        val = reverseL2.val;
+      }
+
+      if (carry != 0) {
+        val++;
+        if (val >= 10) {
+          val = val % 10;
+        }
+      }
+      carry = curCarry;
+      p.next = new ListNode(val);
+      p = p.next;
+
+      if (reverseL1 != null) {
+        reverseL1 = reverseL1.next;
+      }
+
+      if (reverseL2 != null) {
+        reverseL2 = reverseL2.next;
+      }
+    }
+
+    return dummy.next;
+  }
+
+  private ListNode reverse(ListNode head) {
+    if (head == null || head.next == null) {
+      return head;
+    }
+
+    ListNode last = reverse(head.next);
+    head.next.next = head;
+    head.next = null;
+    return last;
+  }
+
+}
