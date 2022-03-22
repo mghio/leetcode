90e7b8e5b646d42e9165e71185b1fe8a12943f1b
mghio <43461943+mghio@users.noreply.github.com>
2022-03-22 23:24:54 +0800

    feat(list): Add Two Numbers solution 2.

--- src/main/java/cn/mghio/leetcode/list/AddTwoNumbers.java
+++ src/main/java/cn/mghio/leetcode/list/AddTwoNumbers.java
@@ -73,6 +73,43 @@ public class AddTwoNumbers {
     return dummy.next;
   }
 
+  public ListNode addTwoNumbers2(ListNode l1, ListNode l2) {
+    if (l1 == null || l2 == null) {
+      return l1 == null ? l2 : l1;
+    }
+
+    ListNode head = null;
+    ListNode p = null;
+    int carry = 0;
+    while (l1 != null || l2 != null) {
+      int v1 = l1 != null ? l1.val : 0;
+      int v2 = l2 != null ? l2.val : 0;
+      int sum = v1 + v2 + carry;
+
+      if (head == null) {
+        head = new ListNode(sum % 10);
+        p = head;
+      } else {
+        p.next = new ListNode(sum % 10);
+        p = p.next;
+      }
+
+      carry = sum / 10;
+      if (l1 != null) {
+        l1 = l1.next;
+      }
+      if (l2 != null) {
+        l2 = l2.next;
+      }
+    }
+
+    if (carry > 0) {
+      p.next = new ListNode(carry);
+    }
+
+    return head;
+  }
+
   private ListNode reverse(ListNode head) {
     if (head == null || head.next == null) {
       return head;
