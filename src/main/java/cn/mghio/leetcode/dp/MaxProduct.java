ed0a3a891686952e95bd3c8e979b3ca1b3519800
mghio <43461943+mghio@users.noreply.github.com>
2022-04-01 23:08:16 +0800

    feat(dp): Maximum Product Subarray solution 2.

--- src/main/java/cn/mghio/leetcode/dp/MaxProduct.java
+++ src/main/java/cn/mghio/leetcode/dp/MaxProduct.java
@@ -31,6 +31,26 @@ public class MaxProduct {
     return ret;
   }
 
+  private int maxProduct2(int[] nums) {
+    if (nums == null || nums.length == 0) {
+      return 0;
+    }
+
+    int max = nums[0];
+    int min = nums[0];
+    int ret = nums[0];
+
+    for (int i = 1, len = nums.length; i < len; i++) {
+      int tempMax = max;
+      int tempMin = min;
+      int curNum = nums[i];
+      max = max(tempMin * curNum, tempMax * curNum, curNum);
+      min = min(tempMin * curNum, tempMax * curNum, curNum);
+      ret = Math.max(max, ret);
+    }
+    return ret;
+  }
+
   private int max(int a, int b, int c) {
     return Math.max(a, Math.max(b, c));
   }
