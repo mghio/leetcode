c392ffed81c4a86edccb6feb81ff4c3185b63f70
mghio <43461943+mghio@users.noreply.github.com>
2022-04-02 16:24:25 +0800

    refactor(array): Shuffle an array.

--- src/main/java/cn/mghio/leetcode/array/ShuffleArray.java
+++ src/main/java/cn/mghio/leetcode/array/ShuffleArray.java
@@ -12,27 +12,32 @@ public class ShuffleArray {
 
   private final int[] nums;
 
-  private final int[] original;
+  private final Random random;
+
+  private final int size;
 
   public ShuffleArray(int[] nums) {
     this.nums = nums;
-    this.original = new int[nums.length];
-    System.arraycopy(nums, 0, original, 0, nums.length);
+    this.random = new Random();
+    this.size = nums.length;
   }
 
   public int[] reset() {
-    System.arraycopy(original, 0, nums, 0, nums.length);
     return nums;
   }
 
   public int[] shuffle() {
-    for (int i = 0, len = nums.length; i < len; i++) {
-      int next = i + new Random().nextInt(len - i);
-      int temp = nums[i];
-      nums[i] = nums[next];
-      nums[next] = temp;
+    int[] rets = nums.clone();
+    for (int i = 0; i < size; i++) {
+      swap(rets, i, i + random.nextInt(size - i));
     }
-    return nums;
+    return rets;
+  }
+
+  private void swap(int[] rets, int i, int j) {
+    int temp = rets[i];
+    rets[i] = rets[j];
+    rets[j] = temp;
   }
 
 }
