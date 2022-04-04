f7414d9e3368a0dbcc558c7c11043bcd418638b7
mghio <43461943+mghio@users.noreply.github.com>
2022-04-04 23:14:58 +0800

    feat(thinking): Palindromic Substrings.

---
+++ src/main/java/cn/mghio/leetcode/thinking/HammingDistance.java
@@ -0,0 +1,21 @@
+package cn.mghio.leetcode.thinking;
+
+/**
+ * Hamming Distance.
+ *
+ * @author mghio
+ * @since 2022-04-04
+ */
+public class HammingDistance {
+
+  public int hammingDistance(int x, int y) {
+    int xorResult = x ^ y;
+    int ret = 0;
+    while (xorResult != 0) {
+      xorResult &= xorResult - 1;
+      ret++;
+    }
+    return ret;
+  }
+
+}
